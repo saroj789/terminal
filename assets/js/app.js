9501a8dedd7fa5a4118ad68e9cc9f4e820640f60
saroj789 <sarojkumarvaishya@gmail.com>
2023-10-02 18:08:58 +0530

    rename

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // terminal
-if (window.location.pathname === "/" || window.location.pathname === "/terminal.html" || window.location.pathname === "/terminal") {
+if (window.location.pathname === "/" || window.location.pathname === "/index.html" || window.location.pathname === "/terminal") {
   new Terminal({
     container: document.querySelector(".terminal .card-body"),
     typingSpeed: 75,
